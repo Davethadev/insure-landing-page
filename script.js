9eee69fcb1451127a9c0034b50e0fac7dfb76445
David Umanah <david_umanah@yahoo.com>
2022-09-30 09:15:09 +0100

    style fix

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const hamburgerIcon = document.querySelector('.hamburger-icon')
 const headerMobileLinks = document.querySelector('.header-mobile-links')
-const headerTextImageWrapper = document.querySelector('.header-text-image-wrapper')
+// const headerTextImageWrapper = document.querySelector('.header-text-image-wrapper')
 // const header = document.querySelector('.header')
-const headerText = document.querySelector('.header-text')
-const imageIntro = document.querySelector('.image-intro-mobile')
+// const headerText = document.querySelector('.header-text')
+// const imageIntro = document.querySelector('.image-intro-mobile')
 
 hamburgerIcon.addEventListener('click', function() {
     headerMobileLinks.classList.toggle('active')
-    headerTextImageWrapper.classList.toggle('active')
+    // headerTextImageWrapper.classList.toggle('active')
     // header.classList.toggle('active')
-    headerText.classList.toggle('active')
-    imageIntro.classList.toggle('active')
+    // headerText.classList.toggle('active')
+    // imageIntro.classList.toggle('active')
     if (this.classList.contains('active')) {
         this.innerHTML = `
         <img src="./images/icon-hamburger.svg" alt="">
